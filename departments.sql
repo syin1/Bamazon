94b82bc6861ebb44753000857a76abdc03dd373c
syin <shaomingyinut@gmail.com>
2018-08-27 07:40:40 -0400

    Created new MySQL table departments

---
+++ departments.sql
@@ -0,0 +1,14 @@
+USE bamazon;
+
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NULL,
+  over_head_costs DECIMAL(10,2) NULL,
+  PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Clothing, Shoes & Jewelry", 300),
+("Electronics", 1000),
+("Books & Audible", 80),
+("Sports & Outdoors", 1500);
